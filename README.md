17c2b6984a7befe2fa153db4199f32f511d17f3c
Gil Pratte <gil.pratte@mutualmobile.com>
2018-08-05 14:50:57 -0500

    custom token authentication

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # security-proof-of-concept
 
-Spring security starts running in basic authentication mode.
+Spring security with configures authentication provider and filter.
 
 Running the following results in a 401 unauthorized status
 ```
@@ -9,5 +9,5 @@ curl localhost:8080/widgets
 
 Running the following (substitute in the basic auth password from the spring boot log when starting) results in a 200 result status
 ```
-curl -u user:99dddba0-ca41-4ee0-ae15-cdfb2a36f060 localhost:8080/widgets
+curl -H "Authorization: Bearer 123456789" localhost:8080/widgets
 ```
---
+++ src/main/java/com/example/poc/security/WebSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.example.poc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import static java.util.Objects.requireNonNull;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final RequestMatcher PUBLIC_URLS = new OrRequestMatcher(
+      new AntPathRequestMatcher("/public/**")
+    );
+    private static final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(PUBLIC_URLS);
+
+
+    TokenAuthenticationProvider provider;
+
+    WebSecurityConfig(final TokenAuthenticationProvider provider) {
+        super();
+        this.provider = requireNonNull(provider);
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(provider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .sessionManagement()
+          .sessionCreationPolicy(STATELESS)
+          .and()
+          .authenticationProvider(provider)
+          .addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class)
+          .authorizeRequests()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .csrf().disable()
+          .formLogin().disable()
+          .httpBasic().disable()
+          .logout().disable();
+    }
+
+    @Bean
+    TokenAuthenticationFilter restAuthenticationFilter() throws Exception {
+        final TokenAuthenticationFilter filter = new TokenAuthenticationFilter(PROTECTED_URLS);
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationSuccessHandler(successHandler());
+        return filter;
+    }
+
+    @Bean
+    SimpleUrlAuthenticationSuccessHandler successHandler() {
+        final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
+        //successHandler.setRedirectStrategy(new NoRedirectStrategy());
+        return successHandler;
+    }
+}
